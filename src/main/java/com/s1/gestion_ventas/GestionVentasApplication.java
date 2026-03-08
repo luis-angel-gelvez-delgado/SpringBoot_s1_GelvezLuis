8c49f6e9ab7e7092a3abe8b53600d3fd5493f274
Luis <lusiangelgelvezdelgado@gmail.com>
2026-03-08 17:22:31 -0500

    feat: :bento: porfin funciona la creacion del .war jsjsjsjs

--- src/main/java/com/s1/gestion_ventas/GestionVentasApplication.java
+++ src/main/java/com/s1/gestion_ventas/GestionVentasApplication.java
@@ -2,12 +2,18 @@ package com.s1.gestion_ventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GestionVentasApplication {
+public class GestionVentasApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestionVentasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GestionVentasApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GestionVentasApplication.class);
+    }
 }
